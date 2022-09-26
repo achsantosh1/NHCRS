6c4de64b1be4406876c38e2fd340b0fee11ded06
Shrawan Poudel <shrwnkr@gmail.com>
2022-09-26 15:59:50 +0545

    temp: fix verbose messages

--- main_file.py
+++ main_file.py
@@ -6,16 +6,14 @@ import string
 from tensorflow.keras.models import load_model
 from flask import Flask, render_template, request
 
-devnagarik_word=['ञ','ट','ठ','ड','ढ','ण','त','थ','द','ध','क','न','प','फ','ब','भ','म','य',
-                 'र','ल','व','ख','श','ष','स','ह','क्ष','त्र','ज्ञ','ग','घ','ङ','च','छ','ज','झ',
-                 '०','१','२','३','४','५','६','७','८','९']
+devnagarik_word = ['ञ', 'ट', 'ठ', 'ड', 'ढ', 'ण', 'त', 'थ', 'द', 'ध', 'क', 'न', 'प', 'फ', 'ब', 'भ', 'म', 'य',
+                   'र', 'ल', 'व', 'ख', 'श', 'ष', 'स', 'ह', 'क्ष', 'त्र', 'ज्ञ', 'ग', 'घ', 'ङ', 'च', 'छ', 'ज', 'झ',
+                   '०', '१', '२', '३', '४', '५', '६', '७', '८', '९']
 newmodel = load_model('Handwritten_OCR.h5')
-
 
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 
 
 def ROI(img):
@@ -36,7 +34,7 @@ def ROI(img):
                 line_seg_img = np.array([])
                 current_r = r
         else:
-                #             print(r)
+            #             print(r)
             if line_seg_img.size <= 1:
                 line_seg_img = np.vstack((np_gray[r], np_gray[r + 1]))
 
@@ -47,23 +45,23 @@ def ROI(img):
 
 
 def preprocessing(img):
-        # resizing the image
+    # resizing the image
     img = cv.resize(img, (800, 600), interpolation=cv.INTER_AREA)
     image_area = img.shape[0] * img.shape[1]
 
-        #     converting into grayscale
+    #     converting into grayscale
     img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     gaussian = cv.GaussianBlur(img_gray, (3, 3), 0)
     _, thresh_img = cv.threshold(gaussian, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
     #     dilated = cv.dilate(thresh_img, None, iterations=1)
 
-        # finding the boundary of the all threshold images
+    # finding the boundary of the all threshold images
     contours, _ = cv.findContours(thresh_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        #     print(len(contours))
+    #     print(len(contours))
     for contour in contours:
-            # boundary of each contour
+        # boundary of each contour
         x, y, w, h = cv.boundingRect(contour)
-            # to discard very small noises
+        # to discard very small noises
         if cv.contourArea(contour) < image_area * 0.0001:
             thresh_img[y:(y + h), x:(x + w)] = 0
         #     cv.imshow('img1',thresh_img)
@@ -72,7 +70,7 @@ def preprocessing(img):
         # line segmentation
     line_segmentation = ROI(thresh_img)
 
-        # word segmentation
+    # word segmentation
     each_word_segmentation = []
     for line in np.asarray(line_segmentation):
         word_segementation = ROI(line.T)
@@ -98,7 +96,7 @@ def dikka_remove(output):
         ig = output[i]
         row, col = ig.shape
 
-                # Detect horizontal lines and removing largest line which is DIKA in word.
+        # Detect horizontal lines and removing largest line which is DIKA in word.
         horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (40, 1))
         detect_horizontal = cv.morphologyEx(ig, cv.MORPH_OPEN, horizontal_kernel, iterations=2)
         cnts, _ = cv.findContours(detect_horizontal, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
@@ -121,7 +119,6 @@ def dikka_remove(output):
     return resultafterdikka, each_character
 
 
-
 @app.route('/')
 def upload_file():
     path = "static/outimg"
@@ -130,13 +127,12 @@ def upload_file():
         pass
     else:
         for i in willremoveimage:
-            os.remove(path+"/"+ i)
+            os.remove(path + "/" + i)
 
-    return render_template('index.html',title='Devnagarik - Home')
+    return render_template('index.html', title='Devnagarik - Home')
 
 
-
-@app.route('/send_pic',methods=['POST'])
+@app.route('/send_pic', methods=['POST'])
 def button_pressed():
     # print("Image recieved")
     dimensions = (100, 100)
@@ -147,31 +143,39 @@ def button_pressed():
 
     name = list(string.ascii_letters)
     word = preprocessing(img)
+    char = ""
     for i in range(len(word)):
         cv.imwrite("static/outimg/image-" + name[i] + ".jpg", word[i])
 
-    for i in range(len(word)):
-        resize = cv.resize(word[i], (32, 32)) / 255.0;
+    total_count = len(word)
+    print("*" * 10)
+    print("Total letters found: ", total_count)
+    for count, i in enumerate(range(total_count), start=1):
+        print("Performing operation for: ", count)
+        resize = cv.resize(word[i], (32, 32)) / 255.0
         reshaped = np.reshape(resize, (1, 32, 32, 1))
 
         prediction = newmodel.predict(reshaped)
-        max = prediction.argmax()
+        score_prediction = prediction > 0.5
+        probab = str(np.amax(prediction))
+        max = score_prediction.argmax()
         predict_character = devnagarik_word[max]
-        char=""
         char += predict_character
-
+        print("Predicted character", predict_character)
+        print("Predicted character index", max)
+        print("Probab", probab)
+        print("*" * 10)
+    print("End !!")
+    print("*" * 10)
     return char
 
-
-@app.route('/uploader', methods=['GET','POST'])
+@app.route('/uploader', methods=['GET', 'POST'])
 def upload():
     path = "static/outimg"
     willremoveimage = os.listdir(path)
     print(willremoveimage)
     for i in willremoveimage:
-        os.remove(path+"/"+ i)
-
-        
+        os.remove(path + "/" + i)
 
     target = os.path.join(APP_ROOT, 'static/images/')
     # print(target)
@@ -181,16 +185,13 @@ def upload():
 
     if request.method == 'POST':
         # for file in request.files.getlist("file"):// for multiple file uplaod
-        file= request.files['file']
+        file = request.files['file']
         filename = file.filename
         destination = "/".join([target, filename])
         # print(destination)
         file.save(destination)
         newDes = os.path.join('static/images/' + filename)
         readingimg = cv.imread(newDes)
-
-
-
 
         def prediction(each_character):
             final_all_word = ""
@@ -209,23 +210,18 @@ def upload():
             return final_all_word
 
         output = preprocessing(readingimg)
-        resultafterdikka, each_character= dikka_remove(output)
+        resultafterdikka, each_character = dikka_remove(output)
         final_result = prediction(each_character)
-
-
-
-
 
         makingimagename = list(string.ascii_letters)
 
         for i in range(len(resultafterdikka)):
-            cv.imwrite("static/outimg/image-"+makingimagename[i]+".jpg", resultafterdikka[i])
+            cv.imwrite("static/outimg/image-" + makingimagename[i] + ".jpg", resultafterdikka[i])
 
         images = os.listdir("static/outimg")
 
-
-    return render_template('index.html',photos=newDes,images_name = images,result=final_result,title='Devnagrik - Predict')
-
+    return render_template('index.html', photos=newDes, images_name=images, result=final_result,
+                           title='Devnagrik - Predict')
 
 
 if __name__ == '__main__':
